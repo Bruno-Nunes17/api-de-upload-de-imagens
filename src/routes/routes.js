e5cfb2bea4cbe51f39862574741ca7882ca6f771
Bruno Nunes <brunooliveiranunes60@gmail.com>
2023-11-16 20:19:01 -0300

    nova rota adicionada

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,5 +4,8 @@ const photosRouter = require("./photos");
 const route = express.Router();
 
 route.use("/photos", photosRouter);
+route.get("/", (req, res) => {
+  res.json({ message: "API online" });
+});
 
 module.exports = route;
